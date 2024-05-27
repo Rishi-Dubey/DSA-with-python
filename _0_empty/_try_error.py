48d819d92b09ace304f81610a329ccf421ea03d8
Rishi Dubey <162816432+Rishi-Dubey@users.noreply.github.com>
2024-05-27 09:13:34 +0530

    Update _try_error.py

--- _0_empty/_try_error.py
+++ _0_empty/_try_error.py
@@ -1,13 +1,1 @@
-class Solution:
-    def twoSum(self, array: list[int], target: int) -> list[int]:
-        flag = 0
-        for i in range(len(array)):
-            for j in range(i + 1, len(array)):
-                if array[i] + array[j] == target:
-                    return [i, j]
-    
-array = [1, 2, 3, 4, 5]
-target = 5
-
-sol =  Solution()
-print(sol.twoSum(array, target))
+# Run your code here for any error
