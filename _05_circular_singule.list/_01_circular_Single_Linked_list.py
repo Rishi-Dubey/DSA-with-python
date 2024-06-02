6074efa53645c2cab7425e02eb9540b452ea2d94
Rishi-Dubey <freefirestars07@gmail.com>
2024-06-02 22:16:55 +0530

    Creating a Circular SLL

---
+++ _05_circular_singule.list/_01_circular_Single_Linked_list.py
@@ -0,0 +1,34 @@
+"""Here I will Create Single Linked list
+1) Head -> connect to node of first and first is connect to Tail"""
+
+class CSLinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+    
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
+                break
+            node = node.next
+        
+    def create(self, value):
+        node = Node(value)
+        self.head = node
+        self.tail = node
+        node.next = node
+        return "CSLL is created Successfully"
+    
+class Node:
+    def __init__(self, value=None) -> None:
+        self.value = value
+        self.next = None
+          
+
+CircularSLL = CSLinkedList()
+
+CircularSLL.create(1)
+print([node.value for node in CircularSLL])
+
