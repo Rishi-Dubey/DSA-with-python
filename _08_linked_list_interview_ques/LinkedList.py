941dd2240a92b5783ac6b30cd86faf308b34157e
Rishi-Dubey <freefirestars07@gmail.com>
2024-06-04 23:00:33 +0530

    add new method : pop

--- _08_linked_list_interview_ques/LinkedList.py
+++ _08_linked_list_interview_ques/LinkedList.py
@@ -85,6 +85,69 @@ class LinkedList:
 
         return self         # return the instance of class itself
 
+    def pop(self, location = -1) -> int | None:
+        """ Return the removed value of given location or None if not found or list is empty
+            It will remove the value at given location(index)
+        """
+
+        if self.head is None:
+            print("Linked List is empty")
+            return None
+
+        else:   # when node/nodes exits
+            # only one node exits
+            if self.head == self.tail and location in (0, -1):
+                value = self.head.value                            # value we will delete
+                self.head = None
+                self.tail = None
+                return value                                       # returning the value we delete
+
+            # More than one nodes exits
+            elif location == 0:                                    # want to remove the first node
+                value = self.head.value                            # value we will delete
+                # now head points to 2nd node
+                self.head = self.head.next
+                return value
+            
+            elif location == -1:                                   # want to remove the last node
+                temp_node = self.head                              # will store the last 2nd node
+                while temp_node.next != self.tail:
+                    temp_node = temp_node.next
+
+                # now we have the 2nd last node(temp_node)
+                # become last so points to None
+                value = temp_node.next.value
+                temp_node.next = None
+                self.tail = temp_node                              # points to new last node
+                return value                                       # returning the value we delete
+
+            else:                                                  # want to delete other than 1st or last node
+                index = 0
+                temp_node = self.head                              # node before the target
+
+                while index < location - 1 and temp_node.next != None:
+                    temp_node = temp_node.next
+                    index += 1
+
+                # 2nd condition means user enters the index more than last node's index
+                # so it out of range
+                if index != location - 1 and temp_node.next is None:     # check if it has valid index
+                    print("Index Out of range")
+                    return None
+
+                # now we have node(temp_node) just before the target
+                target_node = temp_node.next
+                value = target_node.value     # value we will delete
+                
+                # means user want to deleted 
+                if target_node == self.tail:  # means target is last node
+                    temp_node.next = None     # It now becomes last
+                    self.tail = temp_node     # Tail also points to new last node(i.e temp_node)
+                    return value
+                
+                # temp_node next become target node's next(when target is not the last node)
+                temp_node.next = target_node.next
+                return value                  # returning the deleted value
 
 if __name__ == "__main__":
     customLL = LinkedList()
