e35705b134d8f85e62fc647c171ea470253f370e
Rishi-Dubey <freefirestars07@gmail.com>
2024-06-19 17:23:39 +0530

    skip part

---
+++ _14_Avial_tree/_01_Avail_tree.py
@@ -0,0 +1,5 @@
+"""Avail tree is same as Binary search tree but here we need the tree to be balanced
+means the height of leftChild and rightChild should not be more than one
+use -> It make the tree even faster also its operations"""
+
+
