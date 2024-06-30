b9b5d882e6c8af37712a87ae095e42ec0f50f63f
Rishi-Dubey <freefirestars07@gmail.com>
2024-06-30 23:02:26 +0530

    Quick sort

--- _18_sorting_Algorithms/_06_Quick_sort.py
+++ _18_sorting_Algorithms/_06_Quick_sort.py
@@ -1,19 +1,25 @@
-def partition(customList : list, low, high):
-    i = low - 1
-    pivot = customList[high]
-    for j in range(low, high):
-        if customList[j] <= pivot:
+def partition(array : list, initial_index : int, final_index : int):
+    pivot = array[final_index]  # value of final index
+    
+    i = initial_index
+    for j in range(initial_index, final_index):
+        if array[j] <= pivot:
+            array[i], array[j] = array[j], array[i]
             i += 1
-            customList[i], customList[j] = customList[j], customList[i]
-    customList[i + 1], customList[high] = customList[high], customList[i + 1]
-    return i + 1
+    array[i], array[final_index] = array[final_index], array[i]
+    return i
 
-def quickSort(customList : list, low,  high):
-    if low < high:
-        pi = partition(customList, low, high)
-        quickSort(customList, low, pi -1)
-        quickSort(customList, pi+1, high)
+array = [10, 3, 4, 2, 8]
+
+def quickSort(array : list, initial_index, final_index):
+    if initial_index < final_index:
+        sorted_index = partition(array, initial_index, final_index)
+    
+        # well now we know we have sorted the sorted_index in partition function
+        # now we will sort the value to its left and to its right
+        # print(f"II : {initial_index}, FI : {final_index}, TI : {sorted_index}")
+        quickSort(array, initial_index, sorted_index - 1)   # means value just before the sorted index
+        quickSort(array, sorted_index + 1, final_index)  # means value just after the sorted index
         
-array = [10, 11, 4, 6, 2, 8]
-quickSort(array, 0, 5)
+quickSort(array, 0, 4)
 print(array)
